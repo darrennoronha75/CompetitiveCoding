f515d55700b30969c8c6a388cefa950d5604fbe8
darrennoronha75 <darrennoronha1981@gmail.com>
2025-02-27 12:49:30 +0100

    Initial commit - base tufplus folder created

---
+++ problems/tuf_plus/basics/pattern15.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    //Function to print pattren1
+    void pattern15(int n) {
+        
+        // Outer loop will run for rows.
+        for (int i = 0; i < n; i++) {
+            
+            // Inner loop will run for columns.
+            for (int j = 0; j < n; j++) {
+                cout << "*";
+            }
+            /* As soon as n stars are printed, move
+            to the next row and give a line break.*/
+            cout << endl;
+        }
+    }
+
+};
+
+int main() {
+    int N = 4;
+    
+    //Create an instance of the Solution class
+    Solution sol;
+    
+    sol.pattern15(N);
+    
+    return 0;
+}
